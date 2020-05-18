ee7356e88ab8c527f3bf523bb47f09818addaf73
Ci <63220908+grasseconci@users.noreply.github.com>
2020-05-18 14:57:33 +0530

    Remove the hard coded values

--- modules/VpcDemo/variables.tf
+++ modules/VpcDemo/variables.tf
@@ -1,15 +1,14 @@
 variable "vpc_cidr_block"{
-default = "10.0.0.0/16"
-
+  default = ""
 }
 
 variable "public_subnet_cidr"{
-default = "10.0.1.0/24"
+default = ""
 
 }
 
 variable "route_cidr_block"{
-default = "0.0.0.0/0"
+default = ""
 
 }
 
